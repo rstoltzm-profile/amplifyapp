ed0d71a57cb3f20f9493b3bded495104a0e25a50
rstoltzm <rstoltzm@gmail.com>
2021-08-22 12:19:34 -0700

    test

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ function App() {
   }
 
   return (
-    <div className="App">
       <h1>My Notes App</h1>
       <input
         onChange={e => setFormData({ ...formData, 'name': e.target.value})}
